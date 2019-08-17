849d527a4020120e1490af0924ff1b0425634db3
Clara Brígido <clarabrigido@gmail.com>
2019-08-17 20:58:58 +0200

    add cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('>> Ready :)');
+const startBtnElement = document.querySelector('.start-btn');
+let inputElement;
+const cardsListElement = document.querySelector('.list');
+
+function handleSearchClick(){
+  inputElement = document.querySelector('.input__form:checked');
+  const queryNumber = parseInt(inputElement.value);
+  const api = `https://raw.githubusercontent.com/Adalab/cards-data/master/${queryNumber}.json`;
+  //console.log(queryNumber);
+  fetch (api)
+    .then(response => response.json())
+    .then(data => {
+      let searchResult = '';
+      for(const item of data){
+        //if(item.image){
+        searchResult += `<li class="list__item"><img class="image" src="${item.image}" alt"imagen pokemon ${item.name}" width="160" height="195"></li>`;
+        //} else {
+          searchResult += `<li class="list__item"><img class="image" src="https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB" alt"parte trasera de la carta, logo Adalab" height="195" width="160"></li>`;
+       // }
+      }cardsListElement.innerHTML=searchResult;
+    })
+}
+
+startBtnElement.addEventListener('click', handleSearchClick);
